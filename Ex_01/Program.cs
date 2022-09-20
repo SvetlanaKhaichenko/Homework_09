09a969078e7840261a24a0cfb84f7bfa1d01009c
Svetlana <svdorohova@gmail.com>
2022-09-20 13:06:24 +0300

    add Ex-01

---
+++ Ex_01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void SequenceNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        for (int i = m; i < n; i++)
+        {
+            Console.Write($"{i}, ");
+
+        }
+        Console.WriteLine($"{n}");
+    }
+    else Console.WriteLine("Промежутка между M и N не существует");
+}
+
+int M = 4; int N = 8;
+SequenceNumbers(M, N);
